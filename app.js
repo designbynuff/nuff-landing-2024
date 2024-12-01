9b4bf0c00d8c1a3a043fa19dd7844930724947ce
designbynuff <info@designbynuff.com>
2024-12-01 01:47:37 -0500

    Getting GIFfy with it
    Gif overlays are live, not sure I love them...might need to make them smaller and also make newer/better ones.
    
    More importantly, added a selected text background colour that is *chef's kiss*. Somehow it never occurred to me that you could do that.

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ window.addEventListener('load', () => {
 
     // When a cursor hovers over a span, show the corresponding image
 
+    // It could be fun to reveal a secret n+1th thing if you hover over all the other things. Like a "well done, you figured it out, here's a video or something"
+
 });
